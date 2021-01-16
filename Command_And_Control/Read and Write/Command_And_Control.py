c47aba8bd579630ad290ba0550557740a33fb899
Pixel8edstudios <50810194+Pixel8edstudios@users.noreply.github.com>
2021-01-16 12:35:48 -0500

    starting files

---
+++ Command_And_Control/Read and Write/Command_And_Control.py
@@ -0,0 +1,19 @@
+import socket
+import threading
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = socket.gethostname()
+port = 2500
+server.bind((address, port))
+server.listen(200)
+def threaded_welcomer():
+    pass
+def threaded_read():
+    pass
+def threaded_write():
+    pass
+def thread_parsers():
+    pass
+def threaded_data_requests():
+    pass
+def threaded_lts():
+    pass
