527c12905b7635a01490703f66481a484c5fad36
Pixel8edstudios <fiverrtechguy@outlook.com>
2021-01-19 00:17:40 -0500

    trying to add linux support to netmas_determine

--- P2P_Drone/drone_base_cogs/scanner_cogs/netmask_determine.py
+++ P2P_Drone/drone_base_cogs/scanner_cogs/netmask_determine.py
@@ -1,5 +1,6 @@
 import subprocess
 import platform
+import netifaces
 from . import *
 from . import get_ip
 def netmask():
@@ -13,11 +14,11 @@ def netmask():
         mask = proc.stdout.readline().rstrip().split(b':')[-1].replace(b' ',b'').decode()
         return mask
     elif platform.system() == "Linux":
-        proc = subprocess.Popen('ifconfig',stdout=subprocess.PIPE)
-        while True:
-            line = proc.stdout.readline()
-            if ip.encode() in line:
-                break
-        mask = line.rstrip().split(b':')[-1].replace(b' ',b'').decode()
-        return mask
+        print(netifaces.interfaces())
+        for iface in netifaces.interfaces():
+            if iface == 'lo' or iface.startswith('vbox'):
+                continue
+            iface_details = netifaces.ifaddresses(iface)
+            if netifaces.AF_INET in iface_details:
+                print (iface_details[netifaces.AF_INET])
 netmask()
