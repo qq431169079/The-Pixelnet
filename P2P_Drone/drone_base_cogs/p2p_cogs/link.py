3d824c2bf38fc6f24bdd180e5756269ee9738b74
Pixel8edstudios <fiverrtechguy@outlook.com>
2021-02-06 17:32:21 -0500

    added a try-except in link mmodule to account for neighborhood scans

--- P2P_Drone/drone_base_cogs/p2p_cogs/link.py
+++ P2P_Drone/drone_base_cogs/p2p_cogs/link.py
@@ -30,7 +30,16 @@ def link(conn, addr, server):
     conn.send(bytes("attempting_send", "utf-8"))
     print("ATTEMPTED SEND")
     while waiting_for_information == True:
-        raw_net_link = conn.recv(2048)
+        try:
+            raw_net_link = conn.recv(2048)
+        except:
+            print("Likely Port Scan")
+            try:
+                conn.close()
+            except:
+                pass
+            finally:
+                sys.exit()
         if raw_net_link:
             print("RECEIVED INFORMATION FROM RAW_NET_LINK")
             net_link = raw_net_link.decode('utf-8')
