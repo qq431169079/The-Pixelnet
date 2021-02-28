8d92585eb7569417e2ff60d623c80897f6b6d280
Pixel8edstudios <50810194+Pixel8edstudios@users.noreply.github.com>
2021-02-28 17:13:24 -0500

    Added a variable for the message, and added a rudimentary error system.

--- P2P_Drone/drone_base_cogs/head_recv.py
+++ P2P_Drone/drone_base_cogs/head_recv.py
@@ -5,19 +5,23 @@ def head_recv(conn):
             message = message_raw.decode('utf-8')
             message = str(message)
             message_split = message.split()
+            message = message_split[2]
             if message_split:
                 if "[!$HEADER$!]" in message_split:
                     if "[$!FOOTER$!]" in message_split:
-                        if len(message_split[2]) == int(message_split[1]):
-                            return "complete"
+                        if len(message) == int(message_split[1]):
+                            if "LOCAL_ERROR" not in message_split:
+                                return message
+                            else:
+                                return ["ATTEMPTED_ERROR_HIJACK", "SECURITY_ALERT"]
                         else:
                             print("Message length does not to match length expected.")
-                            return "MESSAGE_LEN_ERROR"
+                            return ["MESSAGE_LEN_ERROR", "LOCAL_ERROR"]
                     else:
                         print("Footer missing or compromised.")
-                        return "FOOTER_ERROR"
+                        return ["FOOTER_ERROR", "LOCAL_ERROR"]
                 else:
                     print("Header missing or compromised")
-                    return "HEADER_ERROR"
+                    return ["HEADER_ERROR", "LOCAL_ERROR"]
         else:
             return message_raw
