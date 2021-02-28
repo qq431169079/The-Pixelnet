06ad21487626932639646b656b6c0d42491ad5bf
Pixel8edstudios <50810194+Pixel8edstudios@users.noreply.github.com>
2021-02-28 17:14:29 -0500

    Added a rudimentary error response system, and added rudimentary permanence systems, as a basis for the modules being able to read messages from the incoming_link.

--- P2P_Drone/drone_base_cogs/p2p_cogs/incoming_link.py
+++ P2P_Drone/drone_base_cogs/p2p_cogs/incoming_link.py
@@ -1,5 +1,5 @@
 import threading
-import socket
+import os.path
 import sys
 from ..scanner_cogs import *
 from ..scanner_cogs import neighborhood_scanner
@@ -16,11 +16,18 @@ def p2p_welcomer(server):
         print(f"BOT_CONNECTED:{conn}, {addr}")
         if conn:
             if addr:
-                link_drone_thread = threading.Thread(target=link, args=(conn,))
+                link_drone_thread = threading.Thread(target=link, args=(conn, addr))
                 link_drone_thread.name = "drone_link"
                 link_drone_thread.start()
 
-def link(conn):
+def link(conn, addr):
+    addr = str(addr)
+    ip_message_file_name = addr + ".ipmessage"
+    ip_message_file_location = "./permanence_files/ip_messages"
+    ip_message_file_path = os.path.join(ip_message_file_location, ip_message_file_name)
+    print(ip_message_file_path)
+    if not os.path.isdir(ip_message_file_location):
+        os.makedirs(ip_message_file_location, exist_ok=True)
     print("STARTED INCOMING LINK")
     conn.settimeout(5)
     disconnection_counter = 0
@@ -29,6 +36,27 @@ def link(conn):
         net_link = head_recv(conn)
         if net_link:
             print(net_link)
+            if type(net_link) == type([]):
+                if net_link[1] == "LOCAL_ERROR":
+                    print(f"LOCAL_ERROR: {net_link[0]} experienced. Non-fatal.")
+                if net_link[1] == "SECURITY_ALERT":
+                    print(f"SECURITY_ALERT: {net_link[0]} experienced. Non-fatal.")
+            try:
+                file = open(ip_message_file_path, "r")
+            except:
+                try:
+                    file = open(ip_message_file_path, "x")
+                except Exception.error as e:
+                    print(f"UNEXPECTED READ/WRITE ERROR FOR .IPMESSAGE FUNCTIONALITY: {e}")
+                    sys.exit()
+            finally:
+                try:
+                    file = open(ip_message_file_path, "a+")
+                except:
+                    sys.exit()
+            file.write(net_link)
+            file.write("\n")
+            file.close()
         elif not net_link:
             disconnection_counter += 1
         if disconnection_counter == 5:
